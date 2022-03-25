d250b453e855b3e8bddebf2b607920e76438e861
Alexis Cooke <alexiscooke265@gmail.com>
2022-03-25 19:02:56 -0400

    writing our first python script

---
+++ variables.py
@@ -0,0 +1,26 @@
+# this is a comment... hashtag space words
+# this is a print method. Print will echo out anything inside the round brackets to the terminal, when we run the file
+print("This is my very first python script ever!!!")
+
+# variables are placeholders, with dynamic values -> things that can change
+#stored in memory and referenced later
+name = "Alexis"
+age = 19
+haircolour = "dark brown"
+eyecolour = "green"
+
+# Array s are variable on steroids. they let us store many values in one variable -> to help group data
+# this makes our scripting files easier to understand and work it
+# car1 = "volvo"
+# car2 = "toyota"
+# car3 = "ford"
+cars = ["volvo", "toyota", "ford"]
+
+print("My name is " + name)
+
+# input is another python "thing" - it waits with flashing cursor until you type something and hit enter
+alternatename = input("What is YOUR name?")
+
+print("My name is now " + name)
+
+print("My age is " + str(age))
